f941379b251f99f8d67a58825b71442bba0f5324
Fazil <fazifazu01@gmail.com>
2025-04-23 19:49:02 +0400

    api page

--- config/settings.py
+++ config/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    'django.contrib.staticfiles', 
+    'drf_yasg',
     'rest_framework',
     'corsheaders',
     'users',
